e1bf6f9c85e8013f9d82d214e163568bc259f64a
rpbot_mac <71392285+rp-bot@users.noreply.github.com>
2023-11-11 18:26:43 -0500

    downloaded models

--- models/download-models.sh
+++ models/download-models.sh
@@ -1,11 +1,11 @@
-wget https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands.cfg
-wget https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands.weights
+curl -o ./cross-hands.cfg https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands.cfg
+curl -o ./cross-hands.weights https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands.weights
 
-wget https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-tiny.cfg
-wget https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-tiny.weights
+curl -o ./cross-hands-tiny.cfg https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-tiny.cfg
+curl -o ./cross-hands-tiny.weights https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-tiny.weights
 
-wget https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-tiny-prn.cfg
-wget https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-tiny-prn.weights
+curl -o ./cross-hands-tiny-prn.cfg https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-tiny-prn.cfg
+curl -o ./cross-hands-tiny-prn.weights https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-tiny-prn.weights
 
-wget https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-yolov4-tiny.cfg
-wget https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-yolov4-tiny.weights
+curl -o ./cross-hands-yolov4-tiny.cfg https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-yolov4-tiny.cfg
+curl -o ./cross-hands-yolov4-tiny.weights https://github.com/cansik/yolo-hand-detection/releases/download/pretrained/cross-hands-yolov4-tiny.weights
